2291313ba28d1172213805c0d67aa36bd7c45325
Rodrigo Avila <rodrigo@Rodrigos-MacBook-Pro.local>
2023-09-25 16:29:24 -0300

    Feature: Adding backend calls with axios with examples

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-	Visit
-	<a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script lang="ts">
+	import authService from "../api/AuthService"
+
+	let logoutResponse = JSON.stringify({})
+	let sessionResponse = JSON.stringify({})
+
+	const handleSessionRequest = async(): Promise<void> => {
+			const response = await authService.validateUserSession();
+			sessionResponse = JSON.stringify(response.data);
+	}
+	const handleLogout = async (): Promise<void>  => {
+		const response = await authService.logoutUser();
+		logoutResponse = JSON.stringify(response.data);
+	}
+
+</script>
+<style lang="postcss">
+	button {
+		border: 1px solid black;
+	}
+</style>
+
+<div>
+	<h1>OAuth2 and 2FA</h1>
+	<br>
+	<hr>
+	<br>
+	<a href="http://localhost:3000/api/auth/42/login">Login with OAuth2</a>
+	<br>
+	<br>
+	<button on:click={handleLogout}>Logout</button>
+	{logoutResponse}
+</div>
+<br>
+<hr>
+<br>
+<div>
+	<button on:click={handleSessionRequest}>Get Session</button>
+	{sessionResponse}
+</div>
