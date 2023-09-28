fdac78405ab3ab6da0a444ea69f94dcdf8b7aefc
Anderson <ander_alvarenga@hotmail.com>
2023-09-28 09:59:57 -0300

    Feature: PongHeader Component

---
+++ frontend/src/lib/components/PongHeader.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+		let lineClass = 'bg-white h-3 w-full';
+	let chunkClass = 'flex flex-col gap-4 w-full';
+
+</script>	
+
+<div class="w-full flex flex-row gap-10 items-center">
+		<div class={chunkClass}>
+			<div class={lineClass} />
+			<div class={lineClass} />
+		</div>
+		<p class="text-5xl">PONG</p>
+		<div class={chunkClass}>
+			<div class={lineClass} />
+			<div class={lineClass} />
+		</div>
+	</div>
--- frontend/src/routes/welcome/+page.svelte
+++ frontend/src/routes/welcome/+page.svelte
@@ -2,8 +2,7 @@
 	import { goto } from '$app/navigation';
 	import Image from '$lib/components/Image.svelte';
 	import Camera from '$lib/components/Camera.svelte';
-	let lineClass = 'bg-white h-3 w-full';
-	let chunkClass = 'flex flex-col gap-4 w-full';
+	import PongHeader from '$lib/components/PongHeader.svelte';
 
 	function onWelcome() {
 		//goto('http://localhost:3001/');
@@ -28,17 +27,7 @@
 </script>
 
 <div class="gap-20 flex flex-col pt-10 items-center">
-	<div class="w-full flex flex-row gap-10 items-center">
-		<div class={chunkClass}>
-			<div class={lineClass} />
-			<div class={lineClass} />
-		</div>
-		<p class="text-5xl">PONG</p>
-		<div class={chunkClass}>
-			<div class={lineClass} />
-			<div class={lineClass} />
-		</div>
-	</div>
+	<PongHeader/>
 </div>
 <div class="w-2/3 mx-auto">
 	<div class="gap-5 flex flex-col pt-10 items-center">
