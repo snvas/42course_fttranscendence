65bd22ec152ce3d11d34b5a513ccab7261d6f70f
Anderson <ander_alvarenga@hotmail.com>
2023-10-15 14:54:57 -0300

    Fix: buttons on userlist

--- frontend/src/lib/components/FriendStatus.svelte
+++ frontend/src/lib/components/FriendStatus.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-		import { goto } from '$app/navigation';
+	import { goto } from '$app/navigation';
 
 	type User = {
 		//oponentID: string;
@@ -42,6 +42,14 @@
 			status: 'Playing',
 			friend: false,
 			blocked: false
+		},
+		{
+			nickname: 'Colega',
+			id: '14',
+			avatar: '../../camera.png',
+			status: 'Playing',
+			friend: false,
+			blocked: false
 		}
 	];
 	async function onChat() {
@@ -49,11 +57,11 @@
 	}
 </script>
 
-{#each users as user, i}
+{#each users as user}
 	<div
-		class="w-full border-b border-opacity-20 border-white flex flex-row p-4 gap-4 justify-between items-center"
+		class="w-full border-b border-opacity-20 border-white flex flex-row p-2 gap-4 justify-between items-center"
 	>
-		<div class="flex flex-row gap-4 ml-4 items-center">
+		<div class="flex flex-row gap-4 items-center">
 			<img
 				class="avatar max-w-sm aspect-square w-10 h-10"
 				src={user.avatar}
@@ -62,24 +70,45 @@
 			/>
 			<div class="flex flex-col justify-start">
 				<p class="flex flex-col">{user.nickname}</p>
-				<div class="flex gap-2 text-xs">
-					<p class=" text-green-600">{user.status}</p>
+				{#if user.blocked}
+					<div class="text-red-800 text-xs">Blocked</div>
+				{:else}
+					<p class=" text-green-600 text-xs">{user.status}</p>
 					{#if user.friend}
-						<div class="text-gray-600">|</div>
-						<div class="text-gray-600">Friend</div>
+						<div class="text-gray-600 text-xs">Friend</div>
 					{/if}
-					{#if user.blocked}
-						<div class="text-gray-600">|</div>
-						<div class="text-red-800">Blocked</div>
-					{/if}
-				</div>
+				{/if}
 			</div>
 		</div>
-			<div class="flex flex-row items-center gap-4 text-center text-xs ml-10 justify-end">
-				<button class="w-10"> <img src="/adicionar-usuario.png" alt="add friend" width="90%"/><p class="text-center"> FRIEND</p></button>
-				<button class="w-10" on:click={onChat}> <img src="/bate-papo-de-texto.png" alt="let's chat" /><p class="text-center">  CHAT </p></button>
-				<button class="w-10"> <img src="/bloqueado.png" alt="block this user" width="90%"/><p class="text-center">  BLOCK </p></button>
-				<button class="w-10"> <img src="/pingue-pongue.png" alt="let's play" /><p class="text-center">  PLAY </p></button>
-			</div>
+		<div class="flex flex-row items-center gap-1 text-center text-xs justify-end flex-wrap">
+			{#if !user.blocked}
+				{#if !user.friend}
+					<button class="w-10 bg-white bg-opacity-0 hover:bg-opacity-20 rounded-lg p-1">
+						<img src="/adicionar-usuario.png" alt="add friend" width="90%" />
+						<p class="text-center">FRIEND</p>
+					</button>
+				{/if}
+				<button
+					class="w-10 bg-white bg-opacity-0 hover:bg-opacity-20 rounded-lg p-1"
+					on:click={onChat}
+				>
+					<img src="/bate-papo-de-texto.png" alt="let's chat" />
+					<p class="text-center">CHAT</p>
+				</button>
+				<button class="w-10 bg-white bg-opacity-0 hover:bg-opacity-20 rounded-lg p-1">
+					<img src="/bloqueado.png" alt="block this user" width="90%" />
+					<p class="text-center">BLOCK</p>
+				</button>
+				<button class="w-10 bg-white bg-opacity-0 hover:bg-opacity-20 rounded-lg p-1">
+					<img src="/pingue-pongue.png" alt="let's play" />
+					<p class="text-center">PLAY</p>
+				</button>
+			{:else}
+				<button class="w-10 bg-white bg-opacity-0 hover:bg-opacity-20 rounded-lg p-1">
+					<img src="/bloqueado.png" alt="block this user" width="90%" />
+					<p class="text-center">UNBLOCK</p>
+				</button>
+			{/if}
+		</div>
 	</div>
 {/each}
--- frontend/src/lib/components/UsersList.svelte
+++ frontend/src/lib/components/UsersList.svelte
@@ -2,14 +2,11 @@
 	import FriendStatus from './FriendStatus.svelte';
 </script>
 
-<div class="border-4 border-white min-w-fit w-full h-full flex flex-col rounded-3xl">
+<div class="border-4 border-white w-full h-full flex flex-col rounded-3xl pb-2">
 	<div class="flex-none flex justify-center p-3">
 		<div class="w-full mx-auto min-w-fit mb-4 rounded-xl btn-users">USERS</div>
 	</div>
-	<div class="flex-1 w-full">
+	<div class="flex-1 w-full overflow-y-auto">
 		<FriendStatus />
-	</div>
-	<div class="border-4 border-white h-20 m-2 flex items-center justify-center flex-none rounded-xl bg-[#38761d] hover:bg-[#5bb72b]">
-		<p class="text-3xl">ADD FRIEND +</p>
 	</div>
 </div>
