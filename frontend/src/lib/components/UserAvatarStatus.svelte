a1a95c0ba4e3265741fbf559d74aa641f55e35ff
Anderson <ander_alvarenga@hotmail.com>
2023-10-20 19:44:28 -0300

    Feature: new component UserAvatarStatus

---
+++ frontend/src/lib/components/UserAvatarStatus.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import type { PlayerStatusDto } from '$lib/dtos';
+	import type { AxiosResponse } from 'axios';
+	import AvatarImage from './AvatarImage.svelte';
+
+	export let user: PlayerStatusDto;
+	export let getAvatar: (avatarId: number | null) => Promise<AxiosResponse<Blob> | null> | null;
+
+	const statusColor: { [index: string]: any } = {
+		online: 'text-green-500',
+		offline: 'text-gray-500',
+		playing: 'text-yellow-500'
+	};
+</script>
+
+<div class="flex flex-row gap-4 items-center">
+	<div class="w-12">
+		<AvatarImage avatar={getAvatar(user.avatarId ?? null)} />
+	</div>
+	<div class="flex flex-col justify-start">
+		<p class="flex flex-col">{user.nickname}</p>
+		<!-- {#if user.blocked}
+<div class="text-red-800 text-xs">Blocked</div>
+{:else} -->
+		<p class="{statusColor[user.status]} text-xs">{user.status}</p>
+		<!-- {#if user.friend}
+		<div class="text-gray-600 text-xs">Friend</div>
+	{/if} -->
+		<!--{/if} -->
+	</div>
+</div>
--- frontend/src/lib/components/chat/DirectList.svelte
+++ frontend/src/lib/components/chat/DirectList.svelte
@@ -2,7 +2,7 @@
 	import { createEventDispatcher } from 'svelte';
 	import type { PlayerStatusDto } from '$lib/dtos';
 	import { getAvatarFromId } from '$lib/api';
-	import AvatarImage from '$lib/components/AvatarImage.svelte';
+	import UserAvatarStatus from '../UserAvatarStatus.svelte';
 
 	const dispatch = createEventDispatcher();
 
@@ -15,15 +15,7 @@
 			on:click={() => dispatch('select', history.id)}
 			class="border-b-2 border-x-white h-12 pr-2 flex flex-row gap-4 items-center justify-between"
 		>
-			<div class="flex gap-4 items-center">
-				<div class="h-10 w-10">
-					<AvatarImage avatar={getAvatarFromId(history.avatarId ?? null)} />
-				</div>
-
-				<div class="flex flex-col justify-start">
-					<p class="flex">{history.nickname}</p>
-				</div>
-			</div>
+			<UserAvatarStatus user={history} getAvatar={getAvatarFromId}></UserAvatarStatus>
 			<div class="flex flex-row items-center gap-4 text-center text-xs justify-end flex-wrap">
 				<button class="bg-white bg-opacity-0 hover:bg-opacity-20 rounded-lg p-1">
 					<img src="/bloqueado.png" alt="block this user" class="w-6" />
