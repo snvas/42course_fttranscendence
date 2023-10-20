8d6fe5a22270d3d69751034a121cc639854d782a
Soraia Novaes <sonovaes511@gmail.com>
2023-10-19 21:55:52 -0300

    buttons on direct chat and reponsiveness of dashboard

--- frontend/src/routes/dashboard/+page.svelte
+++ frontend/src/routes/dashboard/+page.svelte
@@ -128,7 +128,7 @@
 				<Settings />
 			{/if}
 		</div>
-		<div class="flex flex-col md:w-1/3 w-full h-full md:order-2 order-first gap-10">
+		<div class="flex flex-col lg:w-1/3 w-full h-full md:order-2 order-first gap-10">
 			<Profile bind:profile={loadProfile} {onLogout} {avatar} />
 
 			<div class="flex flex-row items-center h-full">
@@ -144,7 +144,7 @@
 				<Button type="play" />
 			</div>
 		</div>
-		<div class="gap-15 flex flex-col justify-start md:w-1/3 w-full h-full md:order-2 order-last">
+		<div class="gap-15 flex flex-col justify-start lg:w-1/3 w-full h-full md:order-2 order-last">
 			<UsersList
 				users={$onlineUsers}
 				getAvatar={getAvatarFromId}
