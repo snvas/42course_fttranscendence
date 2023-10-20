102b2a2f09d4e305319e2e3f821ff6e5499f0862
Anderson <ander_alvarenga@hotmail.com>
2023-10-20 20:49:40 -0300

    Fix: dtos import path

--- frontend/src/lib/dtos/index.ts
+++ frontend/src/lib/dtos/index.ts
@@ -3,11 +3,11 @@ export * from '../../../../backend/src/auth/models/one-time-password.dto';
 export * from '../../../../backend/src/user/models/forty-two-user.dto';
 export * from '../../../../backend/src/profile/models/profile.dto';
 export * from '../../../../backend/src/profile/models/profile-delete-response.dto';
-export * from '../../../../backend/src/chat/dto/private-message-history.dto';
-export * from '../../../../backend/src/chat/dto/private-message.dto';
-export * from '../../../../backend/src/chat/dto/conversation.dto';
-export * from '../../../../backend/src/chat/dto/message-profile.dto';
-export * from '../../../../backend/src/chat/dto/player-status.dto';
+export * from '../../../../backend/src/chat/models/private-message-history.dto';
+export * from '../../../../backend/src/chat/models/private-message.dto';
+export * from '../../../../backend/src/chat/models/conversation.dto';
+export * from '../../../../backend/src/chat/models/message-profile.dto';
+export * from '../../../../backend/src/chat/models/player-status.dto';
 
 export type ComponentMessage = {
 	message: string;
