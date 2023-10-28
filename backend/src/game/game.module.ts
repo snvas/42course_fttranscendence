1ac3cec1f14fae4f84c2f422858610fc7b90e10e
Rodrigo Avila <rodrigo@Rodrigos-MacBook-Pro.local>
2023-10-28 20:48:08 -0300

    adding game module dependencies

--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GameGateway } from './game.gateway';
+import { GameController } from './game.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import entities from '../db/entities';
 
 @Module({
-  providers: [GameGateway]
+  controllers: [GameController],
+  providers: [GameGateway],
+  imports: [TypeOrmModule.forFeature(entities)],
+  exports: [],
 })
 export class GameModule {}
