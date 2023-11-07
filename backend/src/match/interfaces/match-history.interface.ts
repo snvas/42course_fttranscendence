8e1d235eb5155ec15641922322fb61c357e176c5
Rodrigo Avila <rodrigo@Rodrigos-MacBook-Pro.local>
2023-11-07 18:29:49 -0300

    Adding initial match history model

--- backend/src/match/interfaces/match-history.interface.ts
+++ backend/src/match/interfaces/match-history.interface.ts
@@ -9,15 +9,6 @@ export interface MatchHistory {
   p2Joined: boolean;
   p1Score: number;
   p2Score: number;
-}
-
-export class MatchHistoryDto implements MatchHistory {
-  id: string;
-  status: 'waitingPlayers' | 'onGoing' | 'finished';
-  p1: SimpleProfileDto;
-  p2: SimpleProfileDto;
-  p1Joined: boolean;
-  p2Joined: boolean;
-  p1Score: number;
-  p2Score: number;
+  createdAt: Date;
+  updatedAt: Date;
 }
