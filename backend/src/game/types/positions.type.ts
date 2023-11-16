2fac3eadf87231ff39cddea2d210b759e89279d6
Alef <alefgabrielr@gmail.com>
2023-11-16 12:52:25 -0300

    cleam position file type

--- backend/src/game/types/positions.type.ts
+++ backend/src/game/types/positions.type.ts
@@ -2,20 +2,3 @@ export type Positions = {
     positionX: number
     positionY: number
 }
-
-export type Player = {
-    id: string
-    soketId: string
-    pos: Positions
-}
-
-export type GameData = {
-    matchId: string
-    pos: Positions
-    userId: string
-}
-
-export type ConsultData = {
-    matchId: string
-    userId: string
-}
