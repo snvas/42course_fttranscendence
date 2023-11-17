34fa4f8e9021ba20e249ae6b96a8aedb35242535
Felipe Camargo <lfelipe_camargo@hotmail.com>
2023-11-16 21:17:26 -0300

    done

--- frontend/src/routes/endgame/+page.svelte
+++ frontend/src/routes/endgame/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { useAuth } from '$lib/stores';
+	import { matchMakingService } from '$lib/api';
+	import { useAuth, match } from '$lib/stores';
 	import PongHeader from '$lib/components/PongHeader.svelte';
-	import player1 from "$lib/assets/avatar-test-1.png"
-	import player2 from "$lib/assets/avatar-test-2.png"
+	import type { MatchHistoryDto } from '../../../../backend/src/match/models/match-history.dto';
+	import AvatarImage from '$lib/components/AvatarImage.svelte';
+	import { getAvatarFromId } from '$lib/api';
 
 	let auth = useAuth();
 
@@ -15,6 +17,16 @@
 		goto('/dashboard');
 	}
 
+	let lastMatch : MatchHistoryDto | null = null;
+
+	async function getMatchHistory() {
+		let response = await matchMakingService.getMatchHistory();
+		let history = response.data;
+
+		lastMatch = history.find((x) => { return x.matchId == $match?.matchId }) ?? null;
+	}
+
+	getMatchHistory()
 </script>
 
 <style>
@@ -32,20 +44,26 @@
 	<PongHeader />
 	<div class="flex flex-col items-center m-14">
 		<div class="border-4 rounded-lg flex flex-col items-center gap-4 h-fit md:p-14 p-12">
-			<p class="md:text-8xl text-6xl lose-color">YOU LOSE</p>
+			{#if lastMatch && lastMatch.winner == "me"}
+				<p class="md:text-8xl text-6xl win-color">YOU WIN</p>
+			{:else}
+				<p class="md:text-8xl text-6xl lose-color">YOU LOSE</p>
+			{/if}
 			<div class="flex flex-row m-5">
 				<div class="flex flex-col items-center">
-					<img src={player1} alt="Player 1" class="md:w-24 md:h-24 w-12 h-12">
-					<p class="mt-4">Player1</p>
+					<div class="md:w-24 md:h-24 w-12 h-12">
+						<AvatarImage avatar={getAvatarFromId(lastMatch?.me.avatarId ?? null)} />
+					</div>
+					<p class="mt-4">{lastMatch?.me.nickname}</p>
 				</div>
-				<p class="md:text-6xl text-2xl m-5">2 x 5</p>
+				<p class="md:text-6xl text-2xl m-5">{lastMatch?.myScore} x {lastMatch?.opponentScore}</p>
 				<div class="flex flex-col items-center">
-					<img src={player2} alt="Player 2" class="md:w-24 md:h-24 w-12 h-12">
-					<p class="mt-4">Player2</p>
+					<div class="md:w-24 md:h-24 w-12 h-12">
+						<AvatarImage avatar={getAvatarFromId(lastMatch?.opponent.avatarId ?? null)} />
+					</div>
+					<p class="mt-4">{lastMatch?.opponent.nickname}</p>
 				</div>
 			</div>
-			<button class="btn-primary" >Play again</button>
-			<button class="btn-primary-yellow" >Rematch</button>
 			<button class="btn-primary-red" on:click={onExit}>Exit</button>
 		</div>
 	</div>
