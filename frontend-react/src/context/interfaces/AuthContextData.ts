84be795d2a36de50ff3dcd24c7d73f28c9b5322d
Anderson <ander_alvarenga@hotmail.com>
2023-11-20 23:04:51 -0300

    Remove frontend_react

--- frontend-react/src/context/interfaces/AuthContextData.ts
+++
@@ -1,9 +0,0 @@
-import { FortyTwoUser } from "../../../../backend/dist/user/interfaces/fortytwo-user.interface";
-
-export interface AuthContextData {
-  user: FortyTwoUser | null;
-  logoutUser: () => void;
-  enable2FA: (code: string) => Promise<boolean>;
-  disable2FA: () => void;
-  validateOTP: (code: string) => Promise<boolean>;
-}
