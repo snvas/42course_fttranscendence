9367ba1ee3ac54e192f1e4bdcf8073c3a2075402
Felipe Camargo <lufelip2@student.42sp.org.br>
2023-11-20 23:03:23 -0300

    Added error page

---
+++ frontend/src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import PongHeader from '$lib/components/PongHeader.svelte';
+</script>
+
+<div class="h-full min-h-screen w-screen">
+	<PongHeader />
+
+	<div class="w-fit mx-auto">
+		<div class="border-4 rounded-xl flex flex-col items-center mt-20 p-20">
+			<p class="text-8xl ">404</p>
+			<p class="text-4xl mt-5">Page not found :/</p>
+		</div>
+	</div>
+</div>
