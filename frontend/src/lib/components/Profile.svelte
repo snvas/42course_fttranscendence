8368755bbc63af7088427bb3eae7cabc67340ed8
Felipe Camargo <lufelip2@student.42sp.org.br>
2023-11-20 22:49:12 -0300

    Added achievements modal and integration with backend

--- frontend/src/lib/components/Profile.svelte
+++ frontend/src/lib/components/Profile.svelte
@@ -4,22 +4,26 @@
 	import AvatarImage from './AvatarImage.svelte';
 	import Button from '$lib/components/Button.svelte';
 	import LevelIndicator from '$lib/components/LevelIndicator.svelte';
-	import { profileService } from '$lib/api'; // Amigos
-	import type { MatchHistoryDto } from '$lib/dtos'; // Partidas
 	export let onLogout: (() => Promise<void>) | null;
 	export let profile: Promise<AxiosResponse<ProfileDTO> | null>;
 	export let avatar: Promise<AxiosResponse<Blob> | null>;
+	import AchievementsModal from '$lib/components/achievementsModal.svelte';
+	import { writable } from 'svelte/store';
+	import { friendsList } from '$lib/stores';
+
+	// Create a writable store
+	export const showAchievements = writable('hide');
+
+	function onShowAllAchievements() {
+		showAchievements.set('show');
+	}
 
 </script>
 
-<style>
-	.achievement-border {
-		border-color: white;
-		border-width: 6px;
-	}
-</style>
-
 <div class="flex flex-col w-full h-full gap-10">
+	{#if $showAchievements == 'show'}
+		<AchievementsModal show={showAchievements}/>
+	{/if}
 	{#await profile}
 		Loading
 	{:then profile}
@@ -45,24 +49,84 @@
 		<LevelIndicator level={profile?.data.level} levelPercentage={profile?.data.level_percentage} />
 
 		<div class="flex flex-row w-full min-w-fit">
-			<p>ACHIEVEMENTS:</p>
+			<p class="text-xl ml-2">ACHIEVEMENTS:</p>
 		</div>
 		<div class="flex flex-row w-full justify-around min-w-fit">
-			<div class="flex flex-col achievement achievement-border">
-				<i class="fa fa-user-plus achievement-icon mb-2" aria-hidden="true"></i>
-				<p class="achievement-text">ADD +5</p>
-				<p class="achievement-text">FRIENDS</p>
-			</div>
-			<div class="flex flex-col achievement achievement-border">
-				<i class="fa fa-trophy achievement-icon mb-2" aria-hidden="true"></i>
-				<p class="achievement-text">WIN +10<p>
-				<p class="achievement-text">ROUNDS</p>
-			</div>
-			<div class="flex flex-col achievement achievement-border">
-				<i class="fa fa-gamepad achievement-icon mb-2" aria-hidden="true"></i>
-				<p class="achievement-text">PLAY +10</p> 
-				<p class="achievement-text">MATCHES</p>
-			</div>
+			{#if $friendsList.length > 30}
+				<button class="flex flex-col achievement achievement-gold" on:click={onShowAllAchievements}>
+					<i class="fa fa-user-plus achievement-icon mb-2" aria-hidden="true"></i>
+					<p >ADD +30</p>
+					<p >FRIENDS</p>
+				</button>
+			{:else if $friendsList.length > 15}
+				<button class="flex flex-col achievement achievement-silver" on:click={onShowAllAchievements}>
+					<i class="fa fa-user-plus achievement-icon mb-2" aria-hidden="true"></i>
+					<p >ADD +15</p>
+					<p >FRIENDS</p>
+				</button>
+			{:else if $friendsList.length > 5}
+				<button class="flex flex-col achievement achievement-bronze" on:click={onShowAllAchievements}>
+					<i class="fa fa-user-plus achievement-icon mb-2" aria-hidden="true"></i>
+					<p >ADD +5</p>
+					<p >FRIENDS</p>
+				</button>
+			{:else}
+				<button class="flex flex-col achievement achievement-none" on:click={onShowAllAchievements}>
+					<i class="fa fa-user-plus achievement-icon mb-2" aria-hidden="true"></i>
+					<p >ADD +5</p>
+					<p >FRIENDS</p>
+				</button>
+			{/if}
+			{#if (profile?.data.wins ?? 0) > 100}
+				<button class="flex flex-col achievement achievement-gold" on:click={onShowAllAchievements}>
+					<i class="fa fa-trophy achievement-icon mb-2" aria-hidden="true"></i>
+					<p >WIN +100<p>
+					<p >ROUNDS</p>
+				</button>
+			{:else if (profile?.data.wins ?? 0) > 30}
+				<button class="flex flex-col achievement achievement-silver" on:click={onShowAllAchievements}>
+					<i class="fa fa-trophy achievement-icon mb-2" aria-hidden="true"></i>
+					<p >WIN +30<p>
+					<p >ROUNDS</p>
+				</button>
+			{:else if (profile?.data.wins ?? 0) > 10}
+				<button class="flex flex-col achievement achievement-bronze" on:click={onShowAllAchievements}>
+					<i class="fa fa-trophy achievement-icon mb-2" aria-hidden="true"></i>
+					<p >WIN +10<p>
+					<p >ROUNDS</p>
+				</button>
+			{:else}
+				<button class="flex flex-col achievement achievement-none" on:click={onShowAllAchievements}>
+					<i class="fa fa-trophy achievement-icon mb-2" aria-hidden="true"></i>
+					<p >WIN +10<p>
+					<p >ROUNDS</p>
+				</button>
+			{/if}
+			{#if (profile?.data.wins ?? 0) + (profile?.data.losses ?? 0) > 100}
+				<button class="flex flex-col achievement achievement-gold" on:click={onShowAllAchievements}>
+					<i class="fa fa-gamepad achievement-icon mb-2" aria-hidden="true"></i>
+					<p >PLAY +100</p>
+					<p >MATCHES</p>
+				</button>
+			{:else if (profile?.data.wins ?? 0) + (profile?.data.losses ?? 0) > 30}
+				<button class="flex flex-col achievement achievement-silver" on:click={onShowAllAchievements}>
+					<i class="fa fa-gamepad achievement-icon mb-2" aria-hidden="true"></i>
+					<p >PLAY +30</p>
+					<p >MATCHES</p>
+				</button>
+			{:else if (profile?.data.wins ?? 0) + (profile?.data.losses ?? 0) > 10}
+				<button class="flex flex-col achievement achievement-bronze" on:click={onShowAllAchievements}>
+					<i class="fa fa-gamepad achievement-icon mb-2" aria-hidden="true"></i>
+					<p >PLAY +10</p>
+					<p >MATCHES</p>
+				</button>
+			{:else}
+				<button class="flex flex-col achievement achievement-none" on:click={onShowAllAchievements}>
+					<i class="fa fa-gamepad achievement-icon mb-2" aria-hidden="true"></i>
+					<p >PLAY +10</p>
+					<p >MATCHES</p>
+				</button>
+			{/if}
 		</div>
 	{/await}
 </div>
---
+++ frontend/src/lib/components/achievementsModal.svelte
@@ -0,0 +1,76 @@
+<script>
+	export let show;
+
+	function onCloseModal() {
+		$show = 'hide';
+	}
+</script>
+
+<div class="relative z-10" aria-labelledby="modal-title" role="dialog" aria-modal="true">
+	<div class="fixed inset-0 bg-gray-900 bg-opacity-95 transition-opacity" />
+	<div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+		<div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+			<div
+			class="relative transform overflow-hidden border-[6px] rounded-lg bg-gray-900 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg p-5"
+			>
+				<div class="flex flex-row justify-between ml-4 mb-5 mr-4">
+					<p class="md:text-3xl text-2xl uppercase">All of this can be yours</p>
+					<button on:click={onCloseModal}>
+						<i class="fa fa-window-close-o md:text-4xl text-3xl icon-link" aria-hidden="true" />
+					</button>
+				</div>
+				<div class="flex flex-row w-full justify-around min-w-fit">
+					<div class="flex flex-col achievement achievement-bronze">
+						<i class="fa fa-user-plus achievement-icon mb-2" aria-hidden="true"></i>
+						<p>ADD +5</p>
+						<p>FRIENDS</p>
+					</div>
+					<div class="flex flex-col achievement achievement-silver">
+						<i class="fa fa-user-plus achievement-icon mb-2" aria-hidden="true"></i>
+						<p>ADD +15</p>
+						<p>FRIENDS</p>
+					</div>
+					<div class="flex flex-col achievement achievement-gold">
+						<i class="fa fa-user-plus achievement-icon mb-2" aria-hidden="true"></i>
+						<p>ADD +30</p>
+						<p>FRIENDS</p>
+					</div>
+				</div>
+				<div class="flex flex-row w-full justify-around min-w-fit mt-5 mb-5">
+					<div class="flex flex-col achievement achievement-bronze">
+						<i class="fa fa-trophy achievement-icon mb-2" aria-hidden="true"></i>
+						<p>WIN +10<p>
+						<p>ROUNDS</p>
+					</div>
+					<div class="flex flex-col achievement achievement-silver">
+						<i class="fa fa-trophy achievement-icon mb-2" aria-hidden="true"></i>
+						<p>WIN +30<p>
+						<p>ROUNDS</p>
+					</div>
+					<div class="flex flex-col achievement achievement-gold">
+						<i class="fa fa-trophy achievement-icon mb-2" aria-hidden="true"></i>
+						<p>WIN +100<p>
+						<p>ROUNDS</p>
+					</div>
+				</div>
+				<div class="flex flex-row w-full justify-around min-w-fit">
+					<div class="flex flex-col achievement achievement-bronze">
+						<i class="fa fa-gamepad achievement-icon mb-2" aria-hidden="true"></i>
+						<p>PLAY +10</p>
+						<p>FRIENDS</p>
+					</div>
+					<div class="flex flex-col achievement achievement-silver">
+						<i class="fa fa-gamepad achievement-icon mb-2" aria-hidden="true"></i>
+						<p>PLAY +30<p>
+						<p>ROUNDS</p>
+					</div>
+					<div class="flex flex-col achievement achievement-gold">
+						<i class="fa fa-gamepad achievement-icon mb-2" aria-hidden="true"></i>
+						<p>PLAY +100</p>
+						<p>MATCHES</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
