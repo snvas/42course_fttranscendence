5318577e7edbd039f1343e979721af5db39ee423
Alef <alefgabrielr@gmail.com>
2023-11-23 21:56:11 -0300

    rm controller

--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { GameGateway } from './game.gateway';
 import { GameService } from './game.service';
-import { GameController } from './game.controller';
 import { MatchModule } from '../match/match.module';
 
 /**
@@ -11,7 +10,6 @@ import { MatchModule } from '../match/match.module';
 @Module({
   imports: [MatchModule],
   providers: [GameGateway, GameService],
-  controllers: [GameController],
   exports: [GameGateway, GameService],
 })
 export class GameModule {}
